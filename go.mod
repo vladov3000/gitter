3255d23668c3d0b1665ac94e8be6acc0327302d8
Vladimir Ovechkin <vladov3000@gmail.com>
2024-06-07 20:14:46 -0700

    First commit.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitter
+
+go 1.19
+
+require github.com/eatonphil/gosqlite v0.9.0 // indirect
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"github.com/eatonphil/gosqlite"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	index      *template.Template
+}
+
+func makeServer() (*Server, error) {
+	index, error := template.ParseFiles("index.html")
+	if error != nil {
+		return nil, error
+	}
+
+	return &Server{index}, nil
+}
+
+func (server *Server) getIndex(writer http.ResponseWriter, request *http.Request) {
+	log.Print("Getting index.")
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	connection, error := gosqlite.Open("gitter.db")
+	if error != nil {
+		log.Print("Failed to open database connection: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+	connection.BusyTimeout(5 * time.Second)
+	defer connection.Close();
+
+	listPosts, error := connection.Prepare(`SELECT content FROM posts LIMIT 10`)
+	if error != nil {
+		log.Print("Failed to prepare statement: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	error = listPosts.Exec()
+	if error != nil {
+		log.Print("Failed to list posts: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	var posts []string
+	for {
+		hasRow, error := listPosts.Step()
+		if error != nil {
+			log.Print("Failed to list posts: ", error)
+			http.Error(writer, "Server error", http.StatusInternalServerError)
+			return
+		}
+		
+		if !hasRow {
+			break
+		}
+
+		var post string
+		error = listPosts.Scan(&post)
+		if error != nil {
+			log.Print("Failed to list posts: ", error)
+			http.Error(writer, "Server error", http.StatusInternalServerError)
+			return
+		}
+		posts = append(posts, post)
+	}
+	
+	index := server.index
+	error = index.Execute(writer, posts)
+	if error != nil {
+		log.Print("Error while executing template: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (server *Server) submitPost(writer http.ResponseWriter, request *http.Request) {
+	log.Print("Submitting post.")
+	if request.Method != http.MethodPost {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	error := request.ParseForm()
+	if error != nil {
+		http.Error(writer, "Malformed form parameters", http.StatusBadRequest)
+		return
+	}
+
+	form := request.PostForm
+	post, ok := form["message"]
+	if !ok {
+		http.Error(writer, "Missing message form parameter", http.StatusBadRequest)
+		return
+	}
+
+	connection, error := gosqlite.Open("gitter.db")
+	if error != nil {
+		log.Print("Failed to open database connection: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+	connection.BusyTimeout(5 * time.Second)
+	defer connection.Close();
+
+	insertPost, error := connection.Prepare(`INSERT INTO posts VALUES (?, ?)`)
+	if error != nil {
+		log.Print("Failed to prepare statement: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+	defer insertPost.Close()
+
+	id := rand.Int63()
+	error = insertPost.Exec(id, post[0])
+	if error != nil {
+		log.Print("Failed to insert post: ", error)
+		http.Error(writer, "Server error", http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(writer, request, "/", http.StatusSeeOther)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	
+	server, error := makeServer()
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	http.HandleFunc("/", server.getIndex)
+	http.HandleFunc("/post", server.submitPost)
+	http.Handle("/static/", http.FileServer(http.Dir(".")))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
